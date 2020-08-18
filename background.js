c69206bd29ecfc35132835ea65270f5090c6a325
Ambrala <ambralaju@gmail.com>
2020-08-18 17:30:23 +0200

    Add files via upload

---
+++ background.js
@@ -0,0 +1,114 @@
+let numBalls = 13;
+let spring = 0.05;
+let gravity = 0.10;
+let friction = -1.0;
+let balls = [];
+let img = []
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+
+}
+
+
+
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0,0);
+  canvas.style('z-index','-1');
+
+    img = new Image();
+    img.src = "lib/pic/day03.png";
+ 
+	
+	
+for (let i = 0; i < numBalls; i++) {
+    balls[i] = new Ball(
+      random(width),
+      random(height),
+      random(50, 90),
+      i,
+      balls
+    );
+  }
+  noStroke();
+  fill(255, 204);
+	
+  }
+
+
+
+
+
+function draw() {
+	background('#99CCFF');
+	
+  balls.forEach(ball => {
+    ball.collide();
+    ball.move();
+    ball.display();
+  });
+}
+
+class Ball {
+  constructor(xin, yin, din, idin, oin) {
+    this.x = xin;
+    this.y = yin;
+    this.vx = 0;
+    this.vy = 0;
+    this.diameter = din;
+    this.id = idin;
+    this.others = oin;
+  }
+
+  collide() {
+    for (let i = this.id + 1; i < numBalls; i++) {
+      // console.log(others[i]);
+      let dx = this.others[i].x - this.x;
+      let dy = this.others[i].y - this.y;
+      let distance = sqrt(dx * dx + dy * dy);
+      let minDist = this.others[i].diameter / 2 + this.diameter / 2;
+      //   console.log(distance);
+      //console.log(minDist);
+      if (distance < minDist) {
+        //console.log("2");
+        let angle = atan2(dy, dx);
+        let targetX = this.x + cos(angle) * minDist;
+        let targetY = this.y + sin(angle) * minDist;
+        let ax = (targetX - this.others[i].x) * spring;
+        let ay = (targetY - this.others[i].y) * spring;
+        this.vx -= ax;
+        this.vy -= ay;
+        this.others[i].vx += ax;
+        this.others[i].vy += ay;
+      }
+    }
+  }
+
+  move() {
+    this.vy += gravity;
+    this.x += this.vx;
+    this.y += this.vy;
+    if (this.x + this.diameter / 2 > width) {
+      this.x = width - this.diameter / 2;
+      this.vx *= friction;
+    } else if (this.x - this.diameter / 2 < 0) {
+      this.x = this.diameter / 2;
+      this.vx *= friction;
+    }
+    if (this.y + this.diameter / 2 > height) {
+      this.y = height - this.diameter / 2;
+      this.vy *= friction;
+    } else if (this.y - this.diameter / 2 < 0) {
+      this.y = this.diameter / 2;
+      this.vy *= friction;
+    }
+  }
+
+  display() {
+   drawingContext.drawImage(img,this.x, this.y, this.diameter, this.diameter);
+		
+  }
+}
+
